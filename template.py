3043f392cf1ca49f1424961d1e348ff1e45fc0e2
demohari <kishore_koneru@yahoo.com>
2022-12-07 15:22:11 +0000

    reformat

--- template.py
+++ template.py
@@ -2,33 +2,33 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s: ")
 
 package_name = "deepClassifier"
 
 list_of_files = [
-   ".github/workflows/.gitkeep",
-   f"src/{package_name}/__init__.py", 
-   f"src/{package_name}/components/__init__.py", 
-   f"src/{package_name}/utils/__init__.py", 
-   f"src/{package_name}/config/__init__.py", 
-   f"src/{package_name}/pipeline/__init__.py", 
-   f"src/{package_name}/entity/__init__.py", 
-   f"src/{package_name}/constants/__init__.py",
-   "tests/__init__.py",
-   "tests/unit/__init__.py",
-   "tests/integration/__init__.py",
-   "configs/config.yaml",
-   "dvc.yaml",
-   "params.yaml",
-   "init_setup.sh",
-   "requirements.txt", 
-   "requirements_dev.txt",
-   "setup.py",
-   "setup.cfg",
-   "pyproject.toml",
-   "tox.ini",
-   "research/trials.ipynb", 
+    ".github/workflows/.gitkeep",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/components/__init__.py",
+    f"src/{package_name}/utils/__init__.py",
+    f"src/{package_name}/config/__init__.py",
+    f"src/{package_name}/pipeline/__init__.py",
+    f"src/{package_name}/entity/__init__.py",
+    f"src/{package_name}/constants/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "configs/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "research/trials.ipynb",
 ]
 
 for filepath in list_of_files:
@@ -40,7 +40,7 @@ for filepath in list_of_files:
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            pass # create an empty file
+            pass  # create an empty file
             logging.info(f"Creating empty file: {filepath}")
 
     else:
